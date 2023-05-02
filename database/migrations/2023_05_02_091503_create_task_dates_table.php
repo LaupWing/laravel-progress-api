ef7f7d8afa1fbbbf3fd1972dbbef625c32a48fed
LaupWing <loc_strict@hotmail.com>
2023-05-02 11:17:01 +0200

    udpate

---
+++ database/migrations/2023_05_02_091503_create_task_dates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create("task_dates", function (Blueprint $table) {
+         $table->id();
+         $table->timestamps();
+         $table->date("date");
+         $table->boolean("finished");
+         $table->foreignIdFor(
+            \App\Models\Task::class,
+            "task_id"
+         )->constrained("tasks");
+      });
+   }
+
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists("task_dates");
+   }
+};
