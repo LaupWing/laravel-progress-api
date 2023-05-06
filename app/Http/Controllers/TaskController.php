f0acf0a619e3ee121b0cbbe0eddc8ba9cce6f4a2
LaupWing <loc_strict@hotmail.com>
2023-05-06 09:00:25 +0200

    removed unused

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,20 +17,13 @@ class TaskController extends Controller
       return User::find(1)->tasks()->with("taskDates")->get();
    }
 
-   /**
-    * Show the form for creating a new resource.
-    */
-   public function create()
-   {
-      //
-   }
 
    /**
     * Store a newly created resource in storage.
     */
    public function store(Request $request)
    {
-      //
+      
    }
 
    /**
