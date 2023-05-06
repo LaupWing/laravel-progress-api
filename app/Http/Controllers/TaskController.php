8b05997d52276fa79202e3eaeb7034cd260c6d7a
LaupWing <loc_strict@hotmail.com>
2023-05-06 09:12:28 +0200

    validation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -26,7 +26,11 @@ class TaskController extends Controller
    public function store(Request $request)
    {
       $placeholderUser = User::find(1); 
-      
+      $placeholderUser->tasks()->create(
+         $request->validate([
+            "name" => ["required"]
+         ])
+      );
    }
 
    /**
