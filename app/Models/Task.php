f73438a6472e3dd479dad524ad67aef2faf128d8
LaupWing <loc_strict@hotmail.com>
2023-05-06 08:36:37 +0200

    udapte

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Task extends Model
 {
@@ -17,5 +18,12 @@ class Task extends Model
 
    public function user(): BelongsTo {
       return $this->belongsTo(User::class, "user_id");
+   }
+
+   public function taskDates(): HasMany {
+      return $this->hasMany(
+         TaskDate::class,
+         "task_id"
+      );
    }
 }
