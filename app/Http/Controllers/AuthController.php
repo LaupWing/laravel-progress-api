3db93c51dca81fb1aa9e22b685f57e2a0cbe8074
LaupWing <loc_strict@hotmail.com>
2023-05-08 15:43:08 +0200

    udpate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    //
+   public function store(Request $request) {
+      if(!Auth::attempt($request->validate([
+         "email" => ["required", "string", "email"],
+         "password" => ["required", "string"]
+      ]), true)) {
+         throw ValidationException::withMessages([
+            "email" => "Authentication failed"
+         ]);
+      }
+   }
 }
