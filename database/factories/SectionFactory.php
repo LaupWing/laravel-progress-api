4d019f3b0e83d0bdfcaaeebcaf1249cc07dbeca0
LaupWing <loc_strict@hotmail.com>
2023-05-08 14:37:31 +0200

    udapted

--- database/factories/SectionFactory.php
+++ database/factories/SectionFactory.php
@@ -2,13 +2,12 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Section>
  */
-class TaskFactory extends Factory
+class SectionFactory extends Factory
 {
    /**
     * Define the model's default state.
@@ -18,8 +17,7 @@ class TaskFactory extends Factory
    public function definition(): array
    {
       return [
-         "name" => fake()->sentence(1),
-         "user_id" => User::all()->random()->id
+         "name" => fake()->word()
       ];
    }
 }
